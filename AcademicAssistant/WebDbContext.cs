a1fd35fba010a858924204542f4dca76796330db
RownakCodes <rownak.shr@gmail.com>
2024-05-12 00:44:08 +0600

    initalized

---
+++ AcademicAssistant/WebDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AcademicAssistant.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademicAssistant
+{
+    public class WebDbContext :DbContext
+    {
+        DbSet<Test> Tests { get; set; }
+
+        public WebDbContext(DbContextOptions<WebDbContext> options)
+      : base(options)
+        {
+        }
+    }
+}
